e200f048a7b4ddf69ae6ef8fc822a56f73aaf078
Geoff Stanley <geoff.stanley@ec.gc.ca>
2026-06-12 19:39:25 +0000

    improve error message for new1 algorithm

--- sectionate/section.py
+++ sectionate/section.py
@@ -684,7 +684,7 @@ def infer_grid_path(i1, j1, i2, j2, gridlon, gridlat, boundary={"X":"periodic", 
             j_prev, i_prev = j,i
             idx = np.argmin(d_list)
             if d_list[idx] == np.inf:
-                raise RuntimeError(f"At (j,i) = {(i,j)}; all neighbors = {neighbors} are further from the end. Is something wrong with your grid?")
+                raise RuntimeError(f"At (j,i) = {(i,j)}; all neighbors = {neighbors} are further from the end at (j,i) = {(j2, i2)}.")
             (j, i) = neighbors[idx]
         elif algorithm == "new2":
             # On the first step, exclude points that go further away from the endpoint.
